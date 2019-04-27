f4d898b1a14baf1f35ea7cac703f39342e9ba80b
Frank Wallace Potratz de Andrade <frankwallace@edu.univali.br>
2019-04-27 19:07:04 -0300

    added activators

--- Scripts/Activator.cs
+++ Scripts/Activator.cs
@@ -3,19 +3,33 @@ using System;
 
 public class Activator : Area2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    Bidivisor link_to_activate;
+    [Export] bool lever_mode = true;
+    [Export] bool active = false;
 
-    // Called when the node enters the scene tree for the first time.
+    private bool entered = false; 
     public override void _Ready()
     {
-        
+        link_to_activate = (Bidivisor)GetParent().GetChild(GetParent().GetChildren().IndexOf(this) + 1);
+        link_to_activate.active = false;
+    }
+    public override void _PhysicsProcess(float delta){
+        foreach (PhysicsBody2D body in GetOverlappingBodies()){
+            if (entered) return;
+            if (body.HasMethod("GetInput")){
+                ChangeState();
+                entered = true;
+                return;
+            }
+        }
+        if (entered){
+            if (!lever_mode) ChangeState();
+            entered = false;
+        }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void ChangeState(){
+        active = !active;
+        link_to_activate.active = active;
+    }
 }
--- Scripts/Bidivisor.cs
+++ Scripts/Bidivisor.cs
@@ -3,11 +3,13 @@ using System;
 
 public class Bidivisor : Area2D
 {
-    [Export] public bool hidden = false;
+    [Export] public bool active = true;
 	public override void _Process(float delta){
-        foreach (PhysicsBody2D body in GetOverlappingBodies()){
-            if (body.HasMethod("GetInput")){
-                ((PlayerBlock)body).SplitBlock(this);
+        if (active){
+            foreach (PhysicsBody2D body in GetOverlappingBodies()){
+                if (body.HasMethod("GetInput")){
+                    ((PlayerBlock)body).SplitBlock(this);
+                }
             }
         }
     }
